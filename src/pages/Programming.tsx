ca4d257f3d61936e049f65cb2ff8cb6695e1de41
Patrick Auri <patrickaulie@gmail.com>
2022-12-29 23:59:10 +0100

    added ignore list & design

--- src/pages/Programming.tsx
+++ src/pages/Programming.tsx
@@ -8,13 +8,30 @@ export const Programming = () => {
   ]
 
   const getGitHubData = async () => {
+    const ignore = ["N3Bot2021", "SubJapanBot"]
     const response = await fetch(
       "https://api.github.com/users/patrickauri/repos"
     )
       .then((res) => res.json())
       .then((d) => {
-        
-        setGitData(d)
+        const filteredResults = d.filter((v: any) => {
+          console.log(v.name)
+          console.log(ignore.includes(v.name))
+          return !ignore.includes(v.name)
+        })
+        //console.log(d)
+        //console.log(filteredResults)
+
+        // d.filter((v: any) => {
+        //   let f = false
+        //   ignore.forEach((item: string) => {
+        //     if (item === v) {
+        //       f = true
+        //     }
+        //   })
+        //   return f
+        // })
+        setGitData(filteredResults)
         if (gitData) {
         }
       })
@@ -30,15 +47,28 @@ export const Programming = () => {
 
   const CodeCard = ({ data }: CodeCardProps) => {
     return (
-      <div className="code-card">
-        <div className="card-img"></div>
+      <div className={`code-card card-${data.language}`}>
         <div className="card-description">
-          <p>{data.name}</p>
-          <p>{data.html_url}</p>
-          <p>{data.updated_at}</p>
-          <p>{data.homepage}</p>
-          <p>{data.language}</p>
-          <p>{data.topics}</p>
+          <p className="card-title">{data.name}</p>
+          <p className="card-language">{data.language}</p>
+          <p className="card-language">{data.description}</p>
+          <p className="card-updated">{data.updated_at}</p>
+          <p className="card-topics">{data.topics}</p>
+          <div
+            className="card-buttons-wrapper"
+            style={data.homepage ? { gridTemplateColumns: "50% 50%" } : {}}
+          >
+            <a className="card-button" href={data.html_url} target="_blank">
+              See Code
+            </a>
+            {data.homepage ? (
+              <a className="card-button" href={data.homepage} target="_blank">
+                Try
+              </a>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
     )
@@ -48,8 +78,8 @@ export const Programming = () => {
     <div className="content-wrapper">
       <div className="title">Programming</div>
       <div className="code-wrapper">
-        {gitData.map((d: any) => (
-          <CodeCard data={d} />
+        {gitData.map((d: any, i: number) => (
+          <CodeCard key={i} data={d} />
         ))}
       </div>
     </div>
